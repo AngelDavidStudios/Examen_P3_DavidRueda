4d993f746b8ddc3fb386babcb227445639ddc763
Angel David <angeldavidstudios@outlook.com>
2024-07-01 08:03:00 -0500

    Agregado imagenes de iconos y correcion del DTO

--- Examen_PF/DTOs/JokesDTO.cs
+++ Examen_PF/DTOs/JokesDTO.cs
@@ -4,12 +4,12 @@ namespace Examen_PF.DTOs;
 
 public partial class JokesDTO: ObservableObject
 {
-    [ObservableProperty] public int IdJoke;
-    [ObservableProperty] public List<string> Categories;
-    [ObservableProperty] public string CreatedAt;
-    [ObservableProperty] public string IconUrl;
-    [ObservableProperty] public string Id;
-    [ObservableProperty] public string UpdatedAt;
-    [ObservableProperty] public string Url;
-    [ObservableProperty] public string Value;
+    [ObservableProperty] public int idJoke;
+    [ObservableProperty] public List<string> categories;
+    [ObservableProperty] public string createdAt;
+    [ObservableProperty] public string iconUrl;
+    [ObservableProperty] public string id;
+    [ObservableProperty] public string updatedAt;
+    [ObservableProperty] public string url;
+    [ObservableProperty] public string value;
 }
