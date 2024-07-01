8014f508a312bba1a5b6dc7a3b47dc4acdabcedd
Angel David <angeldavidstudios@outlook.com>
2024-07-01 08:09:45 -0500

    Actualzado los servicios de ejecucion

--- Examen_PF/MauiProgram.cs
+++ Examen_PF/MauiProgram.cs
@@ -1,4 +1,6 @@
 ﻿using Examen_PF.Data;
+using Examen_PF.MVVM.ViewModels;
+using Examen_PF.MVVM.Views;
 using Microsoft.Extensions.Logging;
 
 namespace Examen_PF;
@@ -21,6 +23,19 @@ public static class MauiProgram
         DbContext.Dispose();
         
         builder.Services.AddDbContext<AppDbContext>();
+        
+        builder.Services.AddTransient<VMJokeDbList>();
+        builder.Services.AddTransient<VJokeDbList>();
+        
+        builder.Services.AddTransient<VMJokeDbAdd>();
+        builder.Services.AddTransient<VJokeDbAdd>();
+        
+        builder.Services.AddTransient<VMJokeDbEdit>();
+        builder.Services.AddTransient<VJokeDbEdit>();
+        
+        Routing.RegisterRoute(nameof(VJokeDbEdit), typeof(VJokeDbEdit));
+        Routing.RegisterRoute(nameof(VJokeDbAdd), typeof(VJokeDbAdd));
+        
 
 #if DEBUG
         builder.Logging.AddDebug();
